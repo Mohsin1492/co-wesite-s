89877a2b46ae2ff4e8538753dd2a3c9e0c4dce40
Mohsin1492 <eliimohsin4@gmail.com>
2025-12-23 12:35:11 +0500

    Updated

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
   menuToggle.classList.toggle('open');
 });
+
+const toggle = document.getElementById("menu-toggle");
+  const navbar = document.getElementById("navbar");
+
+  toggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+  });
